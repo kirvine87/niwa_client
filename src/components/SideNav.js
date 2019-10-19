6a31fdaae7a5dd4cff9a603f88f9b5556943c886
Billy Mackay <bossmack50@gmail.com>
2019-10-19 13:18:25 +0100

    sidenav working with materialize

---
+++ src/components/SideNav.js
@@ -0,0 +1,39 @@
+import React, {Component} from 'react';
+import {Link} from 'react-router-dom';
+import 'materialize-css/dist/css/materialize.min.css';
+import M from 'materialize-css';
+
+class SideNav extends Component {
+  constructor() {
+    super()
+    this.M = window.M;
+  }
+  componentDidMount() {
+    M.AutoInit();
+  }
+render() {
+  return (
+    <div> <nav>Hello</nav>
+  <ul id="slide-out" className="sidenav">
+    <li><div className="user-view">
+      <div className="background">
+        <img src="images/6804073-nice-backgrounds.jpg" alt="background" />
+      </div>
+      <a href="#user"><img className="circle" src="images/kyle.jpeg" alt="kyle" /></a>
+      <a href="#name"><span className="white-text name">Kyle</span></a>
+      <a href="#email"><span className="white-text email">kyle@gmail.com</span></a>
+    </div></li>
+    <li><a href="#!"><i className="material-icons">cloud</i>First Link With Icon</a></li>
+    <li><a href="#!">Second Link</a></li>
+    <li><div className="divider"></div></li>
+    <li><a className="subheader">Subheader</a></li>
+    <li><a className="waves-effect" href="#!">Third Link With Waves</a></li>
+  </ul>
+  <a href="#" data-target="slide-out" className="sidenav-trigger"><i className="material-icons">menu</i></a>
+    </div>
+  );
+}
+}
+
+
+export default SideNav;
