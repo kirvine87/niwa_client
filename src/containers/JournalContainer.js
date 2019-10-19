9a95aa17a5ae9b1f89f9de9f88d0030c5dc510d6
Billy Mackay <bossmack50@gmail.com>
2019-10-19 20:39:48 +0100

    changes

--- src/containers/JournalContainer.js
+++ src/containers/JournalContainer.js
@@ -1,9 +1,22 @@
-import React from 'react';
+import React, {Component} from 'react';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 
 const JournalContainer = (props) => {
+  render(){
   return (
-    <div> This is the journal container!</div>
+    <Router>
+      <React.Fragment>
+        <Switch>
+
+          <Route exact path="/journal/new" render={() => {
+            return <JournalEntry onSubmit={this.handlePost}/>
+          }}/>
+          
+        </Switch>
+      </React.Fragment>
+    </Router>
   )
+}
 }
 
 export default JournalContainer;
