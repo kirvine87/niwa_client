ccc5f13bc57963e199b6ae4300ad5582671e9dfb
Kyle <kirvine87@gmail.com>
2019-10-19 15:31:41 +0100

    adds fetch data

--- src/containers/MainContainer.js
+++ src/containers/MainContainer.js
@@ -3,16 +3,22 @@ import AddCalories from '../components/mainComponents/AddCalories';
 import AddMood from '../components/mainComponents/AddMood';
 import MoodGraph from '../components/mainComponents/MoodGraph';
 import WeightGraph from '../components/mainComponents/WeightGraph';
+import Request from '../helpers/request';
 
 
 class MainContainer extends Component {
   constructor(props){
     super(props);
     this.state = {
-
-
+      days: []
     }
 
+  }
+
+  componentDidMount(){
+    const request = new Request();
+    request.get('api/days')
+    .then(data => this.setState({days: data._embedded.days}))
   }
 
 
