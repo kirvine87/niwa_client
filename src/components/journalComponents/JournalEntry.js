24f16ade5555685be6bbb297d4b26f0c6daceb68
Billy Mackay <bossmack50@gmail.com>
2019-10-19 21:57:51 +0100

    changes to journal

--- src/components/journalComponents/JournalEntry.js
+++ src/components/journalComponents/JournalEntry.js
@@ -10,25 +10,44 @@ class JournalEntry extends Component{
     this.handleJournaling = this.handleJournaling.bind(this);
   }
 
+
+
   handleJournaling(event){
+    console.log('*****', event.target.value);
     this.setState({journalEntry: event.target.value})
   }
 
+  WordCount(str) {
+    return str.split(" ").length;
+  }
+
   render(){
+
+    let count = 0,
+        length = this.state.value?this.state.value.length:0;
+
     return (
       <div className="row">
     <form className="col s12">
       <div className="row">
         <div className="input-field col s12">
-          <textarea id="textarea1" className="materialize-textarea" value={this.state.journalEntry} onChange={this.handleJournaling}></textarea>
+          <textarea id="textarea1" className="materialize-textarea" data-length="5" value={this.state.journalEntry}
+          onChange={(event)=>this.handleJournaling(event)}>{this.state.value}</textarea>
+          <div>Count: {length}</div>
+          <div>this.state.value: {this.state.value}</div>
           <label htmlFor="textarea1">Write your words</label>
           <button>Archive</button>
         </div>
       </div>
     </form>
   </div>
-    )
+);
   }
 }
+
+// ReactDOM.render(
+//   <WordCount />,
+//   document.getElementById('root')
+// );
 
 export default JournalEntry;
