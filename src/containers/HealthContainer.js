ade4ef149d1a0c5aa4f343e09202d7cd39e8c45b
Stephen Blythe <stephenblythe@gmail.com>
2019-10-19 16:45:48 +0100

    Adds basic containers

---
+++ src/containers/HealthContainer.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const HealthContainer = (props) => {
+  return (
+    <div>Hello this is the health container!</div>
+  )
+}
+
+export default HealthContainer;
---
+++ src/containers/JournalContainer.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const JournalContainer = (props) => {
+  return (
+    <dive> This is the journal container!</div>
+  )
+}
+
+export default JournalContainer;
