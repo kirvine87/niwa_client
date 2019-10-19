ca9c165584ad3478728bb08bee2ed52a8b1bcc00
Billy Mackay <bossmack50@gmail.com>
2019-10-19 17:26:01 +0100

    added journal text inout box

--- src/components/journalComponents/JournalEntry.js
+++ src/components/journalComponents/JournalEntry.js
@@ -2,4 +2,24 @@ import React, {Component} from 'react';
 
 class JournalEntry extends Component{
 
+  componentDidMount() {
+    M.AutoInit();
+  }
+
+  render(){
+    return (
+      <div class="row">
+    <form class="col s12">
+      <div class="row">
+        <div class="input-field col s12">
+          <textarea id="textarea1" class="materialize-textarea"></textarea>
+          <label for="textarea1">Textarea</label>
+        </div>
+      </div>
+    </form>
+  </div>
+    )
+  }
 }
+
+export default JournalEntry;
