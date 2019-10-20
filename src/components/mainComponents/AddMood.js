9311224ed96a36a1f48e2eef6cfa534bf93a9d89
Kyle <kirvine87@gmail.com>
2019-10-20 15:29:19 +0100

    adds add mood

--- src/components/mainComponents/AddMood.js
+++ src/components/mainComponents/AddMood.js
@@ -1,10 +1,59 @@
-import React from 'react';
+import React, {Component} from 'react';
 
-const AddMood = () => {
+class AddMood extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      rating: 0,
+      tag: ""
+    }
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleRating = this.handleRating.bind(this);
+    this.handleTag = this.handleTag.bind(this);
+  }
+
+  handleSubmit(event){
+    event.preventDefault();
+    let newMood = {
+      time: Date.now(),
+      rating: this.state.rating,
+      tag: this.state.tag,
+      day: this.props.day
+    }
+    this.props.onSubmit(newMood, "/")
+  }
+
+  handleRating(event){
+    this.setState({rating: event.target.value})
+  }
+
+  handleTag(event){
+    this.setState({tag: event.target.value})
+  }
+
+  render() {
+    if (!this.props.day) {
+      return "Loading..."
+    }
 
     return (
-      null
+      <React.Fragment>
+      <div>
+      <form onSubmit={this.handleSubmit}>
+      <button type="number" name="Mood" onChange={this.handleRating} value="1">Awful</button>
+      <button type="number" name="Mood" onChange={this.handleRating} value="2">Sad</button>
+      <button type="number" name="Mood" onChange={this.handleRating} value="3">Okay</button>
+      <button type="number" name="Mood" onChange={this.handleRating} value="4">Happy</button>
+      <button type="number" name="Mood" onChange={this.handleRating} value="5">Amazing</button>
+      <input type="text" placeholder="Tag" name="MoodTag" onChange={this.handleTag}  />
+      <button type="submit">Save</button>
+      </form>
+      </div>
+      </React.Fragment>
     )
+  }
+
+
 }
 
 export default AddMood;
