92d08b4e8aa71068afc5fe9cf4e9091bda2086b1
Billy Mackay <bossmack50@gmail.com>
2019-10-22 11:16:46 +0100

    journal archive working with collapsible menu

--- src/components/archiveComponents/JournalArchive.js
+++ src/components/archiveComponents/JournalArchive.js
@@ -7,12 +7,21 @@ const JournalArchive = (props) => {
   }
 
   const entries = props.week.map((day, index) => {
-    return <p key={index} value={day}>{day.journalEntry}</p>
+    return <li key={index} value={day} >
+      <div  className="collapsible-header"><i className="material-icons">menu_book</i>Day {day.id} :{day.date.slice(0,10)}</div>
+      <div className="collapsible-body"><span>{day.journalEntry}</span></div>
+    </li>
+
   })
 
+
+
   return (
+
     <React.Fragment >
-      <p>{entries}</p>
+    <ul className="collapsible">
+      {entries}
+    </ul>
     </React.Fragment >
   )
 }
