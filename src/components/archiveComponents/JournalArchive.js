42d70d637a25193a268c64e8533bdd7d09fc77e8
Billy Mackay <bossmack50@gmail.com>
2019-10-22 11:41:16 +0100

    added detail to collapsible menu in archive

--- src/components/archiveComponents/JournalArchive.js
+++ src/components/archiveComponents/JournalArchive.js
@@ -9,7 +9,10 @@ const JournalArchive = (props) => {
   const entries = props.week.map((day, index) => {
     return <li key={index} value={day} >
       <div  className="collapsible-header"><i className="material-icons">menu_book</i>Day {day.id} :{day.date.slice(0,10)}</div>
-      <div className="collapsible-body"><span>{day.journalEntry}</span></div>
+      <div className="collapsible-body"><span>Journal Entry:</span><p>{day.journalEntry}</p>
+      <p>Water Intake: {day.waterIntake}</p>
+      <p>Calorie Intake: {day.calorieIntake}</p>
+      </div>
     </li>
 
   })
