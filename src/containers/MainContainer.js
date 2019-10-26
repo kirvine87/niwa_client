21cd11d6abc1af39062ac3fd04fa9969ea1fe620
Stephen Blythe <stephenblythe@gmail.com>
2019-10-26 11:52:10 +0100

    Updates MainContainer to use new custom /today route found in server

--- src/containers/MainContainer.js
+++ src/containers/MainContainer.js
@@ -18,7 +18,6 @@ class MainContainer extends Component {
       today: null
     }
     this.handleDayUpdate = this.handleDayUpdate.bind(this);
-    this.isToday = this.isToday.bind(this)
     this.handleNewMood = this.handleNewMood.bind(this);
   }
   handleDayUpdate(id, day, location) {
@@ -27,13 +26,6 @@ class MainContainer extends Component {
   .then(() => {
     window.location = location
   })
-}
-isToday(checkDate){
-  const someDate = new Date(checkDate)
-  const today = new Date()
-  return someDate.getDate() === today.getDate() &&
-  someDate.getMonth() === today.getMonth() &&
-  someDate.getFullYear() === today.getFullYear()
 }
 
 handleNewMood(mood, location){
@@ -48,20 +40,12 @@ handleNewMood(mood, location){
   componentDidMount(){
     const request = new Request();
     const promise1 = request.get('api/days/week')
-    const promise2 = request.get('api/days/latest')
+    const promise2 = request.get('api/days/today')
     Promise.all([promise1, promise2])
     .then((data) => {
       this.setState({week: data[0]})
-      if (this.isToday(data[1][0].date)) {
-        return data[1][0];
-      } else {
-        return request.post('api/days', {
-          date: new Date(),
-          journalEntry: ""
-        });
-      }
+      this.setState({today: data[1][0]})
     })
-    .then(data => this.setState({today: data}))
   }
 
   render(){
